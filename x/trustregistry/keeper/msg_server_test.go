959ec4bfe291d49929d23e6cd8b40727fdc70d09
Pratik <pratikasr@gmail.com>
2024-10-13 12:16:34 +0530

    feat(trustregistry): update API and add CreateTrustRegistry test
    - Update API for CreateTrustRegistry
    - Add test case for CreateTrustRegistry message
    
    Signed-off-by: Pratik <pratikasr@gmail.com>

--- x/trustregistry/keeper/msg_server_test.go
+++ x/trustregistry/keeper/msg_server_test.go
@@ -2,9 +2,9 @@ package keeper_test
 
 import (
 	"context"
-	"testing"
-
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
+	"testing"
 
 	keepertest "github.com/verana-labs/verana-blockchain/testutil/keeper"
 	"github.com/verana-labs/verana-blockchain/x/trustregistry/keeper"
@@ -21,4 +21,69 @@ func TestMsgServer(t *testing.T) {
 	require.NotNil(t, ms)
 	require.NotNil(t, ctx)
 	require.NotEmpty(t, k)
+}
+
+func TestMsgServerCreateTrustRegistry(t *testing.T) {
+	k, ms, ctx := setupMsgServer(t)
+
+	creator := sdk.AccAddress([]byte("test_creator")).String()
+	validDid := "did:example:123456789abcdefghi"
+
+	testCases := []struct {
+		name    string
+		msg     *types.MsgCreateTrustRegistry
+		isValid bool
+	}{
+		{
+			name: "Valid Create Trust Registry",
+			msg: &types.MsgCreateTrustRegistry{
+				Creator:  creator,
+				Did:      validDid,
+				Aka:      "http://example.com",
+				Language: "en",
+				DocUrl:   "http://example.com/doc",
+				DocHash:  "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",
+			},
+			isValid: true,
+		},
+		{
+			name: "Invalid DID",
+			msg: &types.MsgCreateTrustRegistry{
+				Creator:  creator,
+				Did:      "invalid-did",
+				Language: "en",
+				DocUrl:   "http://example.com/doc",
+				DocHash:  "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",
+			},
+			isValid: false,
+		},
+		{
+			name: "Missing Language",
+			msg: &types.MsgCreateTrustRegistry{
+				Creator: creator,
+				Did:     validDid,
+				DocUrl:  "http://example.com/doc",
+				DocHash: "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",
+			},
+			isValid: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			resp, err := ms.CreateTrustRegistry(ctx, tc.msg)
+			if tc.isValid {
+				require.NoError(t, err)
+				require.NotNil(t, resp)
+				// Check if the trust registry was actually created
+				tr, err := k.TrustRegistry.Get(ctx, tc.msg.Did)
+				require.NoError(t, err)
+				require.Equal(t, tc.msg.Did, tr.Did)
+				require.Equal(t, tc.msg.Creator, tr.Controller)
+			} else {
+				require.Error(t, err)
+				require.Nil(t, resp)
+			}
+		})
+	}
 }
