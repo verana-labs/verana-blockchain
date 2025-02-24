94e048ca0035839228b6417f9aa3e0cdbf6e7c3c
pratikasr <pratikasr@gmail.com>
2025-02-24 21:29:04 +0530

    feat(trustdeposit): added Trust Deposit in AddDID msg
    Signed-off-by: pratikasr <pratikasr@gmail.com>

---
+++ x/diddirectory/types/event.go
@@ -0,0 +1,13 @@
+package types
+
+// Event types and attribute keys for the DID Directory module
+const (
+	EventTypeAddDID = "add_did"
+
+	// Attribute keys
+	AttributeKeyDID        = "did"
+	AttributeKeyController = "controller"
+	AttributeKeyExpiration = "expiration"
+	AttributeKeyDeposit    = "deposit"
+	AttributeKeyYears      = "years"
+)
